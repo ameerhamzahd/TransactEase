f5be43dd0e7c6cecc0183074f7f6f1927e30a74f
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-26 16:52:34 +0600

    TransactEase add-money-functionality done.

---
+++ script/addMoney.js
@@ -0,0 +1,18 @@
+const addMoneyBtn = document.getElementById('add-money-btn');
+
+addMoneyBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const amountToAdd = document.getElementById('amount-to-add').value;
+    const amount = parseFloat(amountToAdd);
+    const bankAccountNumber = document.getElementById('bank-account-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    
+    if (bankAccountNumber.length === 11  && pinNumber === '021223') {
+        let availableBalance = document.getElementById('available-balance').innerText;
+        let totalAmount = parseFloat(availableBalance);
+        totalAmount = totalAmount + amount;
+        document.getElementById('available-balance').innerText = totalAmount + '.00';
+    } else {
+        alert('Invalid Information. Try Again!');
+    }
+});
